1c5c30e2589f31744810b274e0aeece958967a0c
Jassiem Moore <jassiemmoore@gmail.com>
2014-05-13 17:17:53 -0400

    syntax fixes.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
   
-  before_filter: set_access_control_headers
-  
+  before_filter :set_access_control_headers
+
   def set_access_control_headers
-	  headers['Access-Control-Allow-Origin'] = ‘*’
-	  headers['Access-Control-Request-Method'] = ‘*’
-	  headers['Access-Control-Allow-Headers'] = ‘*’
-	  headers['Access-Control-Allow-Credentials'] = “true”
+	  headers['Access-Control-Allow-Origin'] = '*'
+	  headers['Access-Control-Request-Method'] = '*'
+	  headers['Access-Control-Allow-Headers'] = '*'
+	  headers['Access-Control-Allow-Credentials'] = 'true'
   end
   
 end
