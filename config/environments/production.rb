28dd2c4f8407a050eafc197aaf8c6e2d59892f6c
Jassiem Moore <jassiemmoore@gmail.com>
2014-06-24 16:41:36 -0400

    fix for fonts.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Grassroot::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -56,6 +56,9 @@ Grassroot::Application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
+
+  config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')  
+  config.assets.precompile += /\.(?:svg|eot|woff|ttf)$/
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
